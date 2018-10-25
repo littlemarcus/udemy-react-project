98d8d92cff2591e77d0801b0e173c756d7c3c643
Marcus Little <mcolelittle@gmail.com>
2018-10-24 22:04:32 -0600

    added Person component, styled Person component

--- src/App.js
+++ src/App.js
@@ -23,19 +23,38 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (e) => {
+    this.setState({
+      persons: [
+        { name: "Marcus", age: 25 },
+        { name: e.target.value, age: 4 },
+        { name: 'Bruno', age: 7 }]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
          <h1>Hi, I'm a React App</h1>
          <p>This is really working!</p>
-         <button onClick={(e) => this.switchNameHandler("Marcus!!")}>Switch Name</button>
+         <button
+          style={style}
+         onClick={(e) => this.switchNameHandler("Marcus!!")}>Switch Name</button>
          <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age} />
          <Person
             name={this.state.persons[1].name}
             age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Pony!")} >My Hobbies: Reading</Person>
+            click={this.switchNameHandler.bind(this, "Pony!")}
+            changed={this.nameChangedHandler} >My Hobbies: Reading</Person>
          <Person
             name={this.state.persons[2].name}
             age={this.state.persons[2].age}/>
